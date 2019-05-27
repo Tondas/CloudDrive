e2eba02e76d723ad3f259061da7af437be00a04f
20x20+20 <rchrdkollar@gmail.com>
2019-05-27 22:42:51 +0200

    Electron simple web app integration

---
+++ Source/CloudDrive.ElectronClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CloudDrive.ElectronClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
