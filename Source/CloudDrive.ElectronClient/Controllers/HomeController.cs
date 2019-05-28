c36fa680600106c7f7a6c0fa616e749b6bfa614f
20x20+20 <rchrdkollar@gmail.com>
2019-05-28 23:43:10 +0200

    simple CPU activity test in the electron app

--- Source/CloudDrive.ElectronClient/Controllers/HomeController.cs
+++ Source/CloudDrive.ElectronClient/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CloudDrive.ElectronClient.Models;
+using ElectronNET.API;
+using System.Threading;
 
 namespace CloudDrive.ElectronClient.Controllers
 {
@@ -12,6 +14,22 @@ namespace CloudDrive.ElectronClient.Controllers
     {
         public IActionResult Index()
         {
+            if (HybridSupport.IsElectronActive)
+            {
+                Electron.IpcMain.On("getCpuActivity", async (args) =>
+                {
+                    while (true)
+                    {
+                        var processes = await Electron.App.GetAppMetricsAsync();
+                        var firstCpuPercentUsage = processes.First().Cpu.PercentCPUUsage;
+
+                        var mainWindow = Electron.WindowManager.BrowserWindows.First();
+                        Electron.IpcMain.Send(mainWindow, "cpuActivityReply", firstCpuPercentUsage);
+
+                        Thread.Sleep(1000);
+                    }
+                });
+            }
             return View();
         }
 
---
+++ Source/CloudDrive.ElectronClient/readme.md
@@ -0,0 +1,6 @@
+# Electron client 
+1. dotnet restore
+2. electronize init
+3. electronize start
+
+npm install and build electron scripts will run automatically 
